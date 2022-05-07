ced9c7f7f07e938b2e38c3c94e5673c6b31d68b2
s24nath <bill24nath@gmail.com>
2022-05-07 21:12:46 +0530

    Structure created

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Fetch DOM Elements in a variable
+let addTaskForm = document.querySelector('#addingTask');
+let taskInputvalue = document.querySelector('#task_input');
+let navTabLinksList = document.querySelectorAll('.nav-link');
+let tasklist = document.querySelectorAll('.list-group-item');
