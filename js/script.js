21891d8d0aa118480f7e7f04e69cc8988df4bf7d
s24nath <bill24nath@gmail.com>
2022-05-20 23:44:47 +0530

    Worked on updating the status of the task

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // Fetch DOM Elements in a variable
-const taskInputForm = document.querySelector('#adding_task');
-const taskInputValue = document.querySelector('#task_input');
-const tabContentList = document.querySelector(`#tab_content_list`);
-const navTabLinksList = document.querySelectorAll('.nav-link');
-const tasklist = document.querySelectorAll('.list-group-item');
+const taskInputForm = document.querySelector('#adding_task'); // Input form
+const taskInputValue = document.querySelector('#task_input'); // Input box in the form for getting the value provided 
+const tabContentList = document.querySelector(`#tab_content_list`); // List of tasks
+const navTabLinksList = document.querySelectorAll('.nav-link'); // Tab links 
 
 let todoListDataContainerGlobal = [];
+let editMode = false;
+let editTaskId;
 
 // Function definition to create task data in Local Storage
 const setToLocalStorage = () => {
@@ -22,14 +23,28 @@ const getFromLocalStorage = () => {
     }
 };
 
+// Function definition for updating the status of the task
+const updateTaskStatus = (currentTask) => {
+    currentTask.addEventListener('dblclick', () => {
+        if(!currentTask.children[0].classList.contains('task-checked')) {
+            currentTask.children[0].classList.add('task-checked');
+            todoListDataContainerGlobal[currentTask.dataset.id].isTaskChecked = true;
+        } else {
+            currentTask.children[0].classList.remove('task-checked');
+            todoListDataContainerGlobal[currentTask.dataset.id].isTaskChecked = false;
+        }
+        setToLocalStorage();
+    });
+};
+
 // Function definition for displaying task list
 const displayTasks = () => {   
-    let currentTaskHTML = ''; 
     let taskListHTML = ''; 
-    todoListDataContainerGlobal.forEach((currentElement) => {
-        taskListHTML += `
-        <li class="list-group-item">
-            <p class="task_text">
+    todoListDataContainerGlobal.forEach((currentElement,index) => {
+        let taskStatus = currentElement.isTaskChecked ? "task-checked" : "";
+        taskListHTML = `
+        <li class="list-group-item" data-id="${index}">
+            <p class="task_text ${taskStatus}">
                 ${currentElement.text}
             </p>
             <button class="btn action-btn">
@@ -41,7 +56,7 @@ const displayTasks = () => {
                 <li>Delete</li>
             </ul>
         </li>
-        `;
+        ` + taskListHTML;
     });
     tabContentList.innerHTML = taskListHTML;
     actionBtnFunctionality();
@@ -51,22 +66,29 @@ const displayTasks = () => {
 document.addEventListener('DOMContentLoaded', function() {
     getFromLocalStorage();
     displayTasks();    
+
+    // Event listner for input form to add and edit each task
     taskInputForm.addEventListener("submit", function(event) {
         event.preventDefault();
         let taskEntered = String(taskInputValue.value);        
         if (taskEntered.length !== 0) {
-            const taskData = {
-                text: taskEntered,
-                isPending: false,
-                uniqueTimeStamp: Date.now()
-            };
-            todoListDataContainerGlobal.push(taskData);
-            setToLocalStorage();
-            taskInputValue.value = "";
+            if(!editMode) {
+                const taskData = {
+                    text: taskEntered,
+                    isTaskChecked: false,
+                    uniqueTimeStamp: Date.now()
+                };
+                todoListDataContainerGlobal.push(taskData);
+                setToLocalStorage();
+                taskInputValue.value = "";
+            } else {
+
+            }
             displayTasks();
         } else {
             console.log("No value");
         }
+
     });
 });
 
@@ -74,24 +96,33 @@ document.addEventListener('DOMContentLoaded', function() {
 const actionBtnFunctionality = () => {
     const actionBtnAll = document.querySelectorAll('.action-btn');
     actionBtnAll.forEach((currentElement) => {
+        let actionList = currentElement.nextElementSibling;
         currentElement.addEventListener('click', (event) => {
             event.stopPropagation();
             actionBtnAll.forEach((currentElement) => {
-                if (currentElement !== event.target) {                    
+                if (event.target !== currentElement) {                    
                     currentElement.nextElementSibling.classList.remove('active');
                 }
             });
-            if (!currentElement.nextElementSibling.classList.contains('active')) {
-                currentElement.nextElementSibling.classList.add('active');
+            if (!actionList.classList.contains('active')) {
+                actionList.classList.add('active');
             } else {
-                currentElement.nextElementSibling.classList.remove('active');
+                actionList.classList.remove('active');
             }
+        });
+        updateTaskStatus(actionList.parentElement);
+        actionList.addEventListener('click', (event) => {
+            if(event.target === actionList.children[0]) {
+                console.log("Clicked on Edit");
+            } else if (event.target === actionList.children[2]) {
+                console.log("Clicked on Delete");
+            } 
+        });
+    });
+    document.addEventListener('click', (event) => {
+        actionBtnAll.forEach((currentElement) => {
+            currentElement.nextElementSibling.classList.remove('active');
         });
     });
 };
 
-document.addEventListener('click', (event) => {
-    actionBtnAll.forEach((currentElement) => {
-        currentElement.nextElementSibling.classList.remove('active');
-    });
-});
